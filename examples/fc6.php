b35080ef952fd5ee4050de51a38e86d76bcad676
Hao <howhow926@gmail.com>
2018-03-22 11:12:14 +0800

    update fc6.php for AELTA
    update fc6.php for AELTA

--- examples/fc6.php
+++ examples/fc6.php
@@ -7,29 +7,31 @@ use ModbusTcpClient\Packet\ResponseFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+//已台達為範例 設定人機介面 Port Host
 $connection = BinaryStreamConnection::getBuilder()
-    ->setPort(5020)
-    ->setHost('127.0.0.1')
+    ->setPort(502)
+    ->setHost('172.16.190.51')
     ->build();
 
-$startAddress = 12288;
-$value = 258; // 0x0102
+//起始記憶體位置
+$startAddress = 14;
+$value = true;
 $packet = new WriteSingleRegisterRequest($startAddress, $value);
-echo 'Packet to be sent (in hex): ' . $packet->toHex() . PHP_EOL;
+echo '發送數據: ' . $packet->toHex() . PHP_EOL;
 
 try {
     $binaryData = $connection->connect()
         ->sendAndReceive($packet);
-    echo 'Binary received (in hex):   ' . unpack('H*', $binaryData)[1] . PHP_EOL;
+    echo '二進制:   ' . unpack('H*', $binaryData)[1] . PHP_EOL;
 
     /* @var $response WriteSingleRegisterResponse */
     $response = ResponseFactory::parseResponseOrThrow($binaryData);
-    echo 'Parsed packet (in hex):     ' . $response->toHex() . PHP_EOL;
-    echo 'Register value parsed from packet:' . PHP_EOL;
+    echo '分析包:     ' . $response->toHex() . PHP_EOL;
+    echo '從數據包解析的寄存器值:' . PHP_EOL;
     print_r($response->getValue());
 
 } catch (Exception $exception) {
-    echo 'An exception occurred' . PHP_EOL;
+    echo '發生異常' . PHP_EOL;
     echo $exception->getMessage() . PHP_EOL;
     echo $exception->getTraceAsString() . PHP_EOL;
 } finally {
