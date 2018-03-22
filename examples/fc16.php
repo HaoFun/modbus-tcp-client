fe0b45531b48d7d62b5d0e9597dd500e8239a6c1
Hao <howhow926@gmail.com>
2018-03-22 11:09:28 +0800

    update fc16.php for AELTA
    update fc16.php for AELTA

--- examples/fc16.php
+++ examples/fc16.php
@@ -8,34 +8,30 @@ use ModbusTcpClient\Utils\Types;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+//已台達為範例 設定人機介面 Port Host
 $connection = BinaryStreamConnection::getBuilder()
-    ->setPort(5020)
-    ->setHost('127.0.0.1')
+    ->setPort(502)
+    ->setHost('172.16.190.51')
     ->build();
 
-$startAddress = 12288;
+//起始記憶體位置
+$startAddress = 10;
 
+//一共設定傳5筆參數 分別對應人機介面 $10 $11 $12 $13 $14
 $registers = [
-    Types::toInt16(10), //000a as word
-    Types::toInt16(-1000), //hex: FC18 as word
-    Types::toInt32(2000), //dec: 2000 -> hex: 7d00 is as 2 word 7D00 0000
+    Types::toInt16(1000),
+    Types::toInt16(1000),
+    Types::toInt16(true),
+    Types::toInt16(true),
+    Types::toInt16(true),
 ];
 $packet = new WriteMultipleRegistersRequest($startAddress, $registers);
-echo 'Packet to be sent (in hex): ' . $packet->toHex() . PHP_EOL;
+echo '發送數據: ' . $packet->toHex() . PHP_EOL;
 
 try {
-    $binaryData = $connection->connect()
-        ->sendAndReceive($packet);
-    echo 'Binary received (in hex):   ' . unpack('H*', $binaryData)[1] . PHP_EOL;
-
-    /* @var $response WriteMultipleRegistersResponse */
-    $response = ResponseFactory::parseResponseOrThrow($binaryData);
-    echo 'Parsed packet (in hex):     ' . $response->toHex() . PHP_EOL;
-    echo 'Registers count written parsed from packet:' . PHP_EOL;
-    print_r($response->getRegistersCount());
-
+    $binaryData = $connection->connect()->sendAndReceive($packet);
 } catch (Exception $exception) {
-    echo 'An exception occurred' . PHP_EOL;
+    echo '發生異常' . PHP_EOL;
     echo $exception->getMessage() . PHP_EOL;
     echo $exception->getTraceAsString() . PHP_EOL;
 } finally {
